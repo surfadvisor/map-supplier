69564e8b266f6fb0daefdb14ef0b4658dabfed54
poznas <misterdannypl@gmail.com>
2019-10-26 16:18:19 +0200

    define GET /map-supplier/rectangle API

--- src/main/java/com/surf/advisor/map/supplier/client/spot/api/SpotApiClient.java
+++ src/main/java/com/surf/advisor/map/supplier/client/spot/api/SpotApiClient.java
@@ -1,8 +1,8 @@
 package com.surf.advisor.map.supplier.client.spot.api;
 
-import com.surf.advisor.map.supplier.client.spot.ClientConfiguration;
+import com.surf.advisor.map.supplier.config.FeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name="${spot.name:spot}", url="${spot.url:http://localhost}", configuration = ClientConfiguration.class)
+@FeignClient(name="spot-client", configuration = FeignConfiguration.class)
 public interface SpotApiClient extends SpotApi {
 }
